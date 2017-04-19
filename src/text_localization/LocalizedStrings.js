289fbade30c4b5dd2dcf62f4f619b93b32a9189f
xkorl17 <Koren@eduroam147-088.nbk.vse.cz>
2017-04-19 17:14:09 +0200

    Add registration

--- src/text_localization/LocalizedStrings.js
+++ src/text_localization/LocalizedStrings.js
@@ -133,6 +133,7 @@ export default new LocalizedStrings({
       title: 'Registrace',
       username: 'Uživatelské jméno',
       password: 'Heslo',
+      verifyPassword: 'Znovu heslo',
       firstname: 'Jméno',
       lastname: 'Přijmení',
       birthdate: 'Datum narození',
