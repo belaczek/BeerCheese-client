a065292d8c94d4a96887ab2a03d4e69faba960d9
David Haska <dhaska93@gmail.com>
2017-04-19 09:40:57 +0200

    Login, business conditions page and contact page

--- src/components/popup/LogIn.js
+++ src/components/popup/LogIn.js
@@ -1,42 +1,75 @@
 import React from 'react';
-import { Modal, ModalBody, Button } from 'reactstrap';
+import { Modal, ModalBody, Button, Form, FormGroup, Input, Label } from 'reactstrap';
 import { connect } from 'react-redux';
 import { login } from '../../actions/auth';
 
+class loginModal extends React.Component {
 
-const loginModal = ({ auth, hideModals, login }) => {
-  const handleAdminLogin = event => {
-    event.preventDefault();
-    login({
-      username: 'admin',
-      password: 'admin'
-    });
-  };
-  
-  const handleUserLogin = event => {
-    event.preventDefault();
-    login({
-      username: 'user',
-      password: 'user'
-    });
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      errors: {}
+    };
+
+    this.handleLogin = this.handleLogin.bind(this);
   };
 
-  // const {auth, user, hidModals} = props;
+  handleLogin = event => {
+    event.preventDefault();
 
-  return (
-    <Modal isOpen={true} toggle={hideModals}>
-      <ModalBody>
-        <Button onClick={handleAdminLogin}>Login as Admin</Button>
-        <Button onClick={handleUserLogin}>Login as User</Button>
-        {auth.isFetching && <p>Loading</p>}
-        {auth.err && <p color="warning">Login failed: {JSON.stringify(auth.err)}</p>}
-      </ModalBody>
-    </Modal>
-  );
-};
+    const { login } = this.props;
+
+    console.log(this.state.username);
+
+    login({
+      username: this.state.username,
+      password: this.state.password
+    });
+
+  };
+
+  updateUsernameValue = event => {
+    this.setState({
+      username: event.target.value
+    });
+  };
+
+  updatePasswordValue = event => {
+    this.setState({
+      password: event.target.value
+    });
+  };
+
+  render() {
+    const { auth, hideModals } = this.props;
+
+    return (
+      <Modal isOpen={true} toggle={hideModals}>
+        <ModalBody>
+          <Form onSubmit={this.handleLogin}>
+            <FormGroup>
+              <Label for="username">Jméno</Label>
+              <Input type="text" name="username" id="username" placeholder="Uživatelské jméno" value={this.state.username} onChange={this.updateUsernameValue} />
+            </FormGroup>
+            <FormGroup>
+              <Label for="password">Heslo</Label>
+              <Input type="password" name="password" id="password" placeholder="Heslo" value={this.state.password} onChange={this.updatePasswordValue} />
+            </FormGroup>
+            <Button type="submit">Přihlásit</Button>
+          </Form>
+          {auth.isFetching && <p>Čekejte prosím</p>}
+          {auth.err && <p color="warning">Chyba přihlášení: {JSON.stringify(auth.err)}</p>}
+        </ModalBody>
+      </Modal>
+    );
+  }
+}
 
 const mapStateToProps = state => ({
   auth: state.auth
 });
 
-export default connect(mapStateToProps, { login })(loginModal);
+export default connect(mapStateToProps, {
+  login
+})(loginModal);
